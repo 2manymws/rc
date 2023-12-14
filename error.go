19cda13fb3482f342d6c9d3410f912a25ec1d21b
k1low <k1low@pepabo.com>
2023-12-14 14:08:26 +0900

    Rename ErrDoNotUseCache to ErrShouldNotUseCache

--- error.go
+++ error.go
@@ -7,8 +7,8 @@ import (
 // ErrCacheNotFound is returned when the cache is not found
 var ErrCacheNotFound error = errors.New("cache not found")
 
-// ErrDoNotUseCache is returned if should not use cache
-var ErrDoNotUseCache error = errors.New("do not use cache")
+// ErrShouldNotUseCache is returned if should not use cache
+var ErrShouldNotUseCache error = errors.New("should not use cache")
 
 // ErrCacheExpired is returned if the cache is expired
 var ErrCacheExpired error = errors.New("cache expired")
