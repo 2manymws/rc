af6d25bf0ef4816630e11441c7fa6e8f2068be07
k1LoW <k1lowxb@gmail.com>
2024-06-13 11:42:48 +0900

    rc does not support websocket.

--- rc.go
+++ rc.go
@@ -87,6 +87,11 @@ func newCacheMw(c Cacher, opts ...Option) *cacheMw {
 
 func (m *cacheMw) Handler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		// rc does not support websocket.
+		if req.Header.Get("Connection") == "Upgrade" && req.Header.Get("Upgrade") == "websocket" {
+			next.ServeHTTP(w, req)
+			return
+		}
 		now := time.Now()
 
 		// Copy the request so that it is not affected by the next handler.
