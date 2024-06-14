7b79e3eeb6ef976dc3bedfc748805d441cc944bd
k1LoW <k1lowxb@gmail.com>
2024-06-14 14:31:58 +0900

    Fix lint warn

--- copybuf.go
+++ copybuf.go
@@ -7,12 +7,12 @@ const copyBufPoolSize = 32 * 1024
 
 var copyBufPool = sync.Pool{New: func() any { return new([copyBufPoolSize]byte) }}
 
-func getCopyBuf() []byte {
+func getCopyBuf() []byte { //nostyle:getters
 	return copyBufPool.Get().(*[copyBufPoolSize]byte)[:]
 }
 func putCopyBuf(b []byte) {
 	if len(b) != copyBufPoolSize {
-		panic("trying to put back buffer of the wrong size in the copyBufPool")
+		panic("trying to put back buffer of the wrong size in the copyBufPool") //nostyle:dontpanic
 	}
 	copyBufPool.Put((*[copyBufPoolSize]byte)(b))
 }
